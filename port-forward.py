193bbd9d721836b0b7dd15bef160c8489a49ebf4
Shubham Kumar <saxena.shubham.2110@gmail.com>
2024-02-22 22:23:37 +0530

    Update port-forward.py

--- port-forward.py
+++ port-forward.py
@@ -23,18 +23,18 @@ def transfer(src, dst, direction):
     while True:
         buffer = src.recv(0x400)
         if len(buffer) == 0:
-            print "[-] No data received! Breaking..."
+            print("[-] No data received! Breaking...")
             break
-        # print "[+] %s:%d => %s:%d [%s]" % (src_address, src_port, dst_address, dst_port, repr(buffer))
+        # print("[+] %s:%d => %s:%d [%s]" % (src_address, src_port, dst_address, dst_port, repr(buffer)))
         if direction:
-            print "[+] %s:%d >>> %s:%d [%d]" % (src_address, src_port, dst_address, dst_port, len(buffer))
+            print("[+] %s:%d >>> %s:%d [%d]" % (src_address, src_port, dst_address, dst_port, len(buffer)))
         else:
-            print "[+] %s:%d <<< %s:%d [%d]" % (dst_address, dst_port, src_address, src_port, len(buffer))
+            print("[+] %s:%d <<< %s:%d [%d]" % (dst_address, dst_port, src_address, src_port, len(buffer)))
         dst.send(handle(buffer))
-    print "[+] Closing connecions! [%s:%d]" % (src_address, src_port)
+    print("[+] Closing connecions! [%s:%d]" % (src_address, src_port))
     src.shutdown(socket.SHUT_RDWR)
     src.close()
-    print "[+] Closing connecions! [%s:%d]" % (dst_address, dst_port)
+    print("[+] Closing connecions! [%s:%d]" % (dst_address, dst_port))
     dst.shutdown(socket.SHUT_RDWR)
     dst.close()
 
@@ -44,15 +44,15 @@ def server(local_host, local_port, remote_host, remote_port, max_connection):
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((local_host, local_port))
     server_socket.listen(max_connection)
-    print '[+] Server started [%s:%d]' % (local_host, local_port)
-    print '[+] Connect to [%s:%d] to get the content of [%s:%d]' % (local_host, local_port, remote_host, remote_port)
+    print('[+] Server started [%s:%d]' % (local_host, local_port))
+    print('[+] Connect to [%s:%d] to get the content of [%s:%d]' % (local_host, local_port, remote_host, remote_port))
     while True:
         local_socket, local_address = server_socket.accept()
-        print '[+] Detect connection from [%s:%s]' % (local_address[0], local_address[1])
-        print "[+] Trying to connect the REMOTE server [%s:%d]" % (remote_host, remote_port)
+        print('[+] Detect connection from [%s:%s]' % (local_address[0], local_address[1]))
+        print("[+] Trying to connect the REMOTE server [%s:%d]" % (remote_host, remote_port))
         remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         remote_socket.connect((remote_host, remote_port))
-        print "[+] Tunnel connected! Tranfering data..."
+        print("[+] Tunnel connected! Tranfering data...")
         # threads = []
         s = threading.Thread(target=transfer, args=(
             remote_socket, local_socket, False))
@@ -62,25 +62,25 @@ def server(local_host, local_port, remote_host, remote_port, max_connection):
         # threads.append(r)
         s.start()
         r.start()
-    print "[+] Releasing resources..."
+    print("[+] Releasing resources...")
     remote_socket.shutdown(socket.SHUT_RDWR)
     remote_socket.close()
     local_socket.shutdown(socket.SHUT_RDWR)
     local_socket.close()
-    print "[+] Closing server..."
+    print("[+] Closing server...")
     server_socket.shutdown(socket.SHUT_RDWR)
     server_socket.close()
-    print "[+] Server shuted down!"
+    print("[+] Server shuted down!")
 
 
 def main():
     if len(sys.argv) != 5:
-        print "Usage : "
-        print "\tpython %s [L_HOST] [L_PORT] [R_HOST] [R_PORT]" % (sys.argv[0])
-        print "Example : "
-        print "\tpython %s 127.0.0.1 8888 127.0.0.1 22" % (sys.argv[0])
-        print "Author : "
-        print "\tShubham"
+        print("Usage : ")
+        print("\tpython %s [L_HOST] [L_PORT] [R_HOST] [R_PORT]" % (sys.argv[0]))
+        print("Example : ")
+        print("\tpython %s 127.0.0.1 8888 127.0.0.1 22" % (sys.argv[0]))
+        print("Author : ")
+        print("\tShubham")
         exit(1)
     LOCAL_HOST = sys.argv[1]
     LOCAL_PORT = int(sys.argv[2])
